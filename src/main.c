8952c4b2fc44bbc4dac925c09c23119ec90e4fe3
florenfechete <florenffechete@gmail.com>
2026-03-13 11:52:44 +0100

    feat: añadir validación de entradas no numéricas
    - Limpieza del buffer con getchar hasta '\n'
    - Restricción de opciones entre '1' y '6'
    - Resolución del TODO anterior

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+//#include <ctype.h>
+
 
 
 /* Programa para gestionar información de estudiantes */
@@ -6,47 +8,55 @@
 
 
 int main() {
-
     int estudiantes[100];
-    int opc;
+    int opcion;
 
 
     printf("------------------------Bienvenido al Sistema de Gestion de Estudiantes------------------------\n\n\n");
 
-    do{
-    printf("`````` Menu Principal ``````\n"
-            "1 - Anadir estudiante \n"
-            "2 - Lista de estudiantes\n"
-            "3 - Buscar estudiante\n"
-            "4 - Eliminar estudiante\n"
-            "5 - Mostrar estadisticas\n"
-            "6 - Salir\n");
 
-        printf("\nTeclee opcion: ");
-        scanf("%d",&opc); // tocar
+    do {
+        printf("`````` Menu Principal ``````\n"
+                "1 - Anadir estudiante \n"
+                "2 - Lista de estudiantes\n"
+                "3 - Buscar estudiante\n"
+                "4 - Eliminar estudiante\n"
+                "5 - Mostrar estadisticas\n"
+                "6 - Salir\n");
 
-    } while (opc <= 0 || opc > 6 );
+        // Validación de entrada con getchar + limpieza de buffer
+        do {
+            printf("\nTeclee una opcion: ");
+            opcion = getchar();
+            if (opcion != '\n') {
+                while (getchar() != '\n');
+            }
+        } while (opcion == '\n');
 
-        switch (opc) {
-            case 1:
-                printf("Aqui introduciremos datos");
-                break;
-            case 2:
-                printf("Aqui es la lista de estudiantes");
-                break;
-            case 3:
-                printf("Aqui buscamos estudiantes");
-                break;
-            case 4:
-                printf("Aqui eliminamos estudiantes");
-                break;
-            case 5:
-                printf("Aqui mostramos estadisticas");
-                break;
-            case 6:
-                printf("Salimos del programa");
-                break;
-            default: ;
-        }
-        return 0;
+    } while (opcion <= '0' || opcion > '6' );
+
+
+    switch (opcion) {
+        case '1':
+            printf("Aqui introduciremos datos");
+            break;
+        case '2':
+            printf("Aqui es la lista de estudiantes");
+            break;
+        case '3':
+            printf("Aqui buscamos estudiantes");
+            break;
+        case '4':
+            printf("Aqui eliminamos estudiantes");
+            break;
+        case '5':
+            printf("Aqui mostramos estadisticas");
+            break;
+        case '6':
+            printf("Salimos del programa");
+            break;
+        default: // Imposible llegar aquí
+            break;
+    }
+    return 0;
 }
