db6b6a7ea6172553c865848c3e065dcfc6b0abc7
florenfechete <florenffechete@gmail.com>
2026-03-17 12:28:57 +0100

    Corregir lógica del menú principal y validación de opciones

--- src/main.c
+++ src/main.c
@@ -8,12 +8,9 @@
 
 
 int main() {
-    int estudiantes[100];
     int opcion;
 
-
     printf("------------------------Bienvenido al Sistema de Gestion de Estudiantes------------------------\n\n\n");
-
 
     do {
         printf("`````` Menu Principal ``````\n"
@@ -31,32 +28,32 @@ int main() {
             if (opcion != '\n') {
                 while (getchar() != '\n');
             }
-        } while (opcion == '\n');
+        } while (opcion == '\n' || opcion <= '0' || opcion >='7'); // ¿Valores ASCII de \n y 0?
 
-    } while (opcion <= '0' || opcion > '6' );
+        switch (opcion) {
+            case '1':
+                printf("Aqui introduciremos datos");
+                break;
+            case '2':
+                printf("Aqui es la lista de estudiantes");
+                break;
+            case '3':
+                printf("Aqui buscamos estudiantes");
+                break;
+            case '4':
+                printf("Aqui eliminamos estudiantes");
+                break;
+            case '5':
+                printf("Aqui mostramos estadisticas");
+                break;
+            case '6':
+                printf("Salimos del programa");
+                break;
+            default: // Imposible llegar aquí
+                break;
+        }
 
+    } while (opcion == '6' );
 
-    switch (opcion) {
-        case '1':
-            printf("Aqui introduciremos datos");
-            break;
-        case '2':
-            printf("Aqui es la lista de estudiantes");
-            break;
-        case '3':
-            printf("Aqui buscamos estudiantes");
-            break;
-        case '4':
-            printf("Aqui eliminamos estudiantes");
-            break;
-        case '5':
-            printf("Aqui mostramos estadisticas");
-            break;
-        case '6':
-            printf("Salimos del programa");
-            break;
-        default: // Imposible llegar aquí
-            break;
-    }
     return 0;
 }
